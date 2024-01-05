d485d589a4c6c2b9fa64c54d33880c51fd2c97c9
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-05 14:47:52 -0300

    add host (correção para deploy no render)

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -6,6 +6,7 @@ const PORT = env.APP_PORT
 app
   .listen({
     port: PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log(`HTTP Server Running on port ${PORT}`)
