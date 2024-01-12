7cd9b2e6f9c962e58c580b9fb41de472c5100e4a
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-12 15:25:29 -0300

    handler de erros global

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -27,7 +27,7 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
       return res.status(409).send({ message: error.message })
     }
 
-    return res.status(500).send() // TODO melhorar essa tratativa
+    throw error
   }
 
   return res.status(201).send()
