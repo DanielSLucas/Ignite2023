d6f14603ee468e7b3bd78ffc9d2516286a96059a
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-22 14:09:05 -0300

    criando test environment

--- 03-api-solid/src/use-cases/search-gyms.spec.ts
+++ 03-api-solid/src/use-cases/search-gyms.spec.ts
@@ -38,7 +38,7 @@ describe('Search gyms Use Case', () => {
     expect(gyms).toEqual([expect.objectContaining({ title: 'Javascript Gym' })])
   })
 
-  it.skip('should be able to fetch paginated gyms search', async () => {
+  it('should be able to fetch paginated gyms search', async () => {
     for (let i = 1; i <= 22; i++) {
       await gymsRepository.create({
         title: `Javascript Gym ${i}`,
--- 03-api-solid/vite.config.ts
+++ 03-api-solid/vite.config.ts
@@ -3,4 +3,14 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    environmentOptions: {
+      adapter: 'psql',
+      envFile: '.env.test',
+      prismaEnvVarName: 'DATABASE_URL', // Optional
+      transformMode: 'ssr', // Optional
+    },
+  },
 })
