ca09d0d6744b330c66d2b2d70d3c1b4777e5fdda
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-23 18:52:33 -0300

    controllers e rotas de check-ins

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,9 +1,12 @@
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
-import { env } from './env'
+
 import { userRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
+
+import { env } from './env'
 
 export const app = fastify()
 
@@ -13,6 +16,7 @@ app.register(fastifyJwt, {
 
 app.register(userRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, res) => {
   if (error instanceof ZodError) {
