6c8eb45e90b59bca558368a45d244e049c54ca86
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-23 18:39:27 -0300

    rotas de academia

---
+++ 03-api-solid/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,26 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/fetch-nearby-gyms-use-case'
+
+export async function nearby(req: FastifyRequest, res: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return res.status(200).send({ gyms })
+}
