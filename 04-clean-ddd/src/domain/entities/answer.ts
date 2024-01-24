0b374f0599077df4e0d4e8ba98deac4a5e8d2915
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-24 16:38:35 -0300

    primeiro caso de uso

--- 04-clean-ddd/src/domain/entities/answer.ts
+++ 04-clean-ddd/src/domain/entities/answer.ts
@@ -1,16 +1,13 @@
 import { randomUUID } from "node:crypto"
 
-export class Question {
+export class Answer {
   public id: string
-  public title: string
   public content: string
 
   constructor(
-    title: string,
     content: string,
     id?: string
   ) {
-    this.title = title
     this.content = content
     this.id = id ?? randomUUID() 
   }
