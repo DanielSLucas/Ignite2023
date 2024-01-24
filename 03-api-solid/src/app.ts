2acebc53b8575bdd9f190c9dc0fe9d8af8731f6e
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-24 09:52:39 -0300

    implementando refresh token

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,4 +1,5 @@
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 
@@ -12,7 +13,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(userRoutes)
 app.register(gymsRoutes)
--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance } from 'fastify'
+
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
+
 import { verifyJWT } from '../../middlewares/verify-jwt'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Rotas autenticadas
   app.get('/me', { onRequest: [verifyJWT] }, profile)
