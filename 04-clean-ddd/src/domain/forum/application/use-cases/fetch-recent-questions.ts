f177c0483999968fc5fac4ed017580df599b075e
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-31 15:03:41 -0300

    Listar perguntas

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,22 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions'
+
+interface FetchRecentQuestionsUseCaseParams {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseReturn {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseParams): Promise<FetchRecentQuestionsUseCaseReturn> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+
+    return { questions }
+  }
+}
--- 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -40,5 +41,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     if (!question) return null
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 }
