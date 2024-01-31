3fbc62f09c68115527f5612cb3d8b5e231783251
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-01-31 16:13:39 -0300

    comentar em uma resposta

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const newAnswer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: newAnswer.id.toString(),
+      authorId: newAnswer.authorId.toString(),
+      content: 'Test comment',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Test comment',
+    )
+  })
+
+  it('should not be able to comment on a non-existent answer', async () => {
+    await expect(() =>
+      sut.execute({
+        answerId: 'non-existent-answer-id',
+        authorId: 'author-1',
+        content: 'Test comment',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
