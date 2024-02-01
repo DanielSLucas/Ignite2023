93b58ca74888b41763597fc7d40c649a49578e48
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-02-01 16:52:10 -0300

    functional error handling

--- 04-clean-ddd/src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,55 +2,59 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
 import { makeAnswerComment } from 'test/factories/make-answer-comment'
+import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from './errors/resource-not-found'
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
 let sut: DeleteAnswerCommentUseCase
 
-describe('Delete Question Comment', () => {
+describe('Delete Answer Comment', () => {
   beforeEach(() => {
     inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
     sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository)
   })
 
-  it('should be able to delete a question comment', async () => {
+  it('should be able to delete a answer comment', async () => {
     const newAnswerComment = makeAnswerComment(
       {
         authorId: new UniqueEntityID('author-1'),
       },
-      new UniqueEntityID('question-comment-1'),
+      new UniqueEntityID('answer-comment-1'),
     )
     await inMemoryAnswerCommentsRepository.create(newAnswerComment)
 
     await sut.execute({
       authorId: 'author-1',
-      answerCommentId: 'question-comment-1',
+      answerCommentId: 'answer-comment-1',
     })
     expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0)
   })
 
-  it('should not be able to delete a question comment if not exists', async () => {
-    await expect(() =>
-      sut.execute({
-        authorId: 'author-1',
-        answerCommentId: 'non-existent-question-comment-id',
-      }),
-    ).rejects.toBeInstanceOf(Error)
+  it('should not be able to delete a answer comment if not exists', async () => {
+    const result = await sut.execute({
+      authorId: 'author-1',
+      answerCommentId: 'non-existent-answer-comment-id',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 
-  it('should not be able to delete a question comment from another user', async () => {
+  it('should not be able to delete a answer comment from another user', async () => {
     const newAnswerComment = makeAnswerComment(
       {
         authorId: new UniqueEntityID('author-1'),
       },
-      new UniqueEntityID('question-comment-1'),
+      new UniqueEntityID('answer-comment-1'),
     )
     await inMemoryAnswerCommentsRepository.create(newAnswerComment)
 
-    await expect(() =>
-      sut.execute({
-        authorId: 'author-2',
-        answerCommentId: 'question-1',
-      }),
-    ).rejects.toBeInstanceOf(Error)
+    const result = await sut.execute({
+      authorId: 'author-2',
+      answerCommentId: 'answer-comment-1',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
--- 04-clean-ddd/src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,11 +1,17 @@
+import { Either, left, right } from '@/core/either'
 import { AnswerCommentsRepository } from '../repositories/answer-comments'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface DeleteAnswerCommentUseCaseParams {
   authorId: string
   answerCommentId: string
 }
 
-type DeleteAnswerCommentUseCaseReturn = void
+type DeleteAnswerCommentUseCaseReturn = Either<
+  ResourceNotFoundError | NotAllowedError,
+  null
+>
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
@@ -18,13 +24,15 @@ export class DeleteAnswerCommentUseCase {
       await this.answerCommentsRepository.findById(answerCommentId)
 
     if (!answerComment) {
-      throw new Error('Question comment not found.')
+      return left(new ResourceNotFoundError())
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed.')
+      return left(new NotAllowedError())
     }
 
     await this.answerCommentsRepository.delete(answerComment)
+
+    return right(null)
   }
 }
