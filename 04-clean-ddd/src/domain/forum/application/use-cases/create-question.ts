731e8d553d19abdada7a63f62fada95f3cb074a4
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-02-02 15:02:33 -0300

    criação de perguntas com anexos

--- 04-clean-ddd/src/domain/forum/application/use-cases/create-question.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions'
 import { Either, right } from '@/core/either'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseParams {
   authorId: string
   title: string
   content: string
+  attachmentsIds: string[]
 }
 
 type CreateQuestionUseCaseReturn = Either<
@@ -23,12 +25,22 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentsIds,
   }: CreateQuestionUseCaseParams): Promise<CreateQuestionUseCaseReturn> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       title,
       content,
     })
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+
+    question.attachments = questionAttachments
 
     await this.questionsRepository.create(question)
 
--- 04-clean-ddd/src/domain/forum/enterprise/entities/question.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question.ts
@@ -4,6 +4,7 @@ import { Slug } from './value-objects/slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { AggregateRoot } from '@/core/entities/aggregate-root'
+import { QuestionAttachment } from './question-attachment'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
@@ -11,6 +12,7 @@ export interface QuestionProps {
   title: string
   content: string
   slug: Slug
+  attachments: QuestionAttachment[]
   createdAt: Date
   updatedAt?: Date
 }
@@ -53,6 +55,14 @@ export class Question extends AggregateRoot<QuestionProps> {
     return this.props.slug
   }
 
+  get attachments() {
+    return this.props.attachments
+  }
+
+  set attachments(attachments: QuestionAttachment[]) {
+    this.props.attachments = attachments
+  }
+
   get createdAt() {
     return this.props.createdAt
   }
@@ -74,13 +84,14 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   static create(
-    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>,
     id?: UniqueEntityID,
   ) {
     const question = new Question(
       {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
+        attachments: props.attachments ?? [],
         createdAt: props.createdAt ?? new Date(),
       },
       id,
